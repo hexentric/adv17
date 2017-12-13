116e906c810cb747b0611f6e93b193e7d560ee0a
hexentric <jhexentric@gmail.com>
2017-12-13 00:02:28 -0700

    day4 part2

--- d4/main.cpp
+++ d4/main.cpp
@@ -4,16 +4,26 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <algorithm>
 
 using namespace std;
 using namespace boost;
 
-bool isValid (const string& passPhrase) {
+bool isValid (const string& passPhrase,
+              bool noAnagrams
+             ) {
   vector<string> fields;
   boost::split( fields, passPhrase, boost::is_any_of(" "));
   map<string,size_t> wordCounts;
   for (auto &el : fields) {
-    ++wordCounts[el];
+    if (noAnagrams) {
+      string sorted = el;
+      std::sort(sorted.begin(), sorted.end());
+      ++wordCounts[sorted];
+    }
+    else {
+      ++wordCounts[el];
+    }
   }
   for (const auto &wc : wordCounts) {
     if (wc.second > 1) { return 0; }
@@ -30,14 +40,19 @@ int main(int argc, char *argv[argc])
   //
   //test = "xmuf znkhaes pggrlp zia znkhaes znkhaes";
   //cout << isValid(test) << endl;
-
+  //
+  //string test = "abcde xyz ecdab";
+  //cout << isValid(test) << endl;
+  
   ifstream infile("input");
   string line;
   int validCount = 0;
+  int noAnagramsCount = 0;
   while (getline(infile, line)) {
-    validCount += isValid( line);
+    validCount += isValid( line, 0);
+    noAnagramsCount += isValid( line, 1);
   }
-  cout << validCount << endl;
-  
+  cout << "part1: " << validCount << endl;
+  cout << "part2: " << noAnagramsCount << endl;
   return 0;
 }
