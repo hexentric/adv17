0515928c509300fcd91fbd92c828333ae8e4dcad
hexentric <jhexentric@gmail.com>
2017-12-12 22:37:30 -0700

    day1 part2

--- d1/main.cpp
+++ d1/main.cpp
@@ -18,8 +18,26 @@ int captcha(std::string str) {
   return sum;
 }
 
+int captcha2(std::string str) {
+  int len = str.size();
+  if (len<2) return 0;
+  if (len%2) return 0; // never odd
+
+  int sum = 0;
+  std::string lh (str, 0, len/2);
+  std::string th (str, len/2);
+  //std::cout << lh << std::endl << th;
+  for (int i=0; i<len/2; i++) {
+    char c1 = lh[i];
+    char c2 = th[i];
+    //std::cout << c1 << ' ' << c2 << std::endl;
+    if (c1==c2) sum+=2*(c1-'0');
+  }
+  return sum;
+}
+
 int main(int argc, char *argv[argc])
 {
-  std::cout << captcha(argv[1]) << std::endl;
+  std::cout << captcha2(argv[1]) << std::endl;
   return 0;
 }
